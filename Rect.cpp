80ec4070c35246e6854eb737a6a181de0f4da815
SeungHuLee <gadlsh12081208@gmail.com>
2018-07-04 12:07:26 +0900

    Implement constructors

--- Rect.cpp
+++ Rect.cpp
@@ -1,7 +1,11 @@
 #include "Rect.h"
 #include <iostream>
 using namespace std;
-Rect::Rect() {
+Rect::Rect(const Point& topLeft, const Point& bottomRight) :_topLeft(topLeft), _bottomRight(bottomRight) {
+	
+}
+
+Rect::Rect(int left, int top, int right, int bottom) : _topLeft(left, top), _bottomRight(right, bottom) {
 
 }
 
